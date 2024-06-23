fb16ec8e13e9daa302812afeb602cb8830f62f1d
aargeee <211220042@nitdelhi.ac.in>
2024-06-23 22:51:23 +0530

    website done

--- src/env.tsx
+++ src/env.tsx
@@ -1,3 +1,2 @@
-export const ACAD_ME_URL = "http://localhost:8000/api";
-export const GOOGLE_RECAPTCHA_KEY = "6LcG2f4pAAAAAJ4f_bmbR9t9HmqgERAFXNLrVy40";
+export const ACAD_ME_URL = "https://aargeee.pythonanywhere.com/api";
 export const PAGINATION_PAGE_SIZE = 10;
